4593d9992d65633ec77a55545092e20543239d48
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-06-19 23:46:51 +0200

    feat(all): initial release

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import {WebpackConfig, get} from '@easy-webpack/core'
+import * as path from 'path'
+
+/**
+ * Json loader support for *.json files.
+ * See: https://github.com/webpack/json-loader
+ */
+export function json(exclude: Array<string> = null) {
+  return function json(this: WebpackConfig): WebpackConfig {
+    return {
+      module: {
+        loaders: get(this, 'module.loaders', []).concat([{
+          test: /\.json$/i,
+          loader: 'json',
+          exclude: exclude || this.metadata.root ? [path.join(this.metadata.root, 'node_modules')] : [],
+        }])
+      }
+    }
+  }
+}
